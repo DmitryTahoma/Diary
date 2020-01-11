12bf318e06893125075783250c1082b0e52d2121
DmitryTahoma <dimon.otrishk@gmail.com>
2020-01-11 21:38:25 +0200

    added user registration on server

--- ServerRealization.Test/ServerCommands.Test.cs
+++ ServerRealization.Test/ServerCommands.Test.cs
@@ -1,4 +1,8 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ServerRealization.Database;
+using ServerRealization.Database.Context;
+using System;
+using System.Linq;
 
 namespace ServerRealization.Test
 {
@@ -20,6 +24,57 @@ namespace ServerRealization.Test
             string result = server.ExecuteCommand(commandName, args);
 
             Assert.AreEqual(expectedResult, result);
+        }
+        
+        [DataTestMethod]
+        [DataRow(new string[] { }, "ae")]
+        [DataRow(new string[] { "" }, "ae")]
+        [DataRow(new string[] { "", "", "" }, "ae")]
+        [DataRow(new string[] { "User", "Password" }, "ae")]
+        [DataRow(new string[] { "", "Password", "Name" }, "ae")]
+        [DataRow(new string[] { "User", "", "Name" }, "ae")]
+        [DataRow(new string[] { "User", "Password", "" }, "ae")]
+        [DataRow(new string[] { "User", "Password", "Name" }, "True")]
+        public void RegisterNewUserTest(string[] args, string expectedResult)
+        {
+            ServerProgram server = new ServerProgram("192.168.0.106", 11221, new int[] { 11222 }, 100);
+            if (args != null)
+                if (args.Length != 0)
+                    if(args[0] != "")
+                        args[0] += DateTime.Now.Second.ToString() + DateTime.Now.Millisecond.ToString();
+
+            DateTime before = DateTime.Now;
+
+            string result = server.ExecuteCommand("rnu", args);
+
+            DateTime after = DateTime.Now;
+
+            Assert.AreEqual(expectedResult, result);
+
+            if(expectedResult == "True")
+            {
+                result = server.ExecuteCommand("clp", args);
+                Assert.AreEqual(expectedResult, result);
+
+                User registeredUser = DBContext.Users.Where(x => x.Id == DBContext.Users.Max(y => y.Id)).First();
+                Assert.AreEqual(args[0], registeredUser.Login);
+                Assert.AreEqual(args[1], registeredUser.Password);
+                Assert.AreEqual(args[2], registeredUser.Name);
+                Assert.IsTrue(before < registeredUser.Registration && registeredUser.Registration < after);
+            }
+        }
+        
+        [TestMethod]
+        public void UniqueCheckerTest()
+        {
+            ServerProgram server = new ServerProgram("192.168.0.106", 11221, new int[] { 11222 }, 100);
+
+            string result = server.ExecuteCommand("rnu", new string[] { 
+                DBContext.Users.Where(x => x.Id == DBContext.Users.Max(y => y.Id)).First().Login,
+                "password", "name"
+            });
+
+            Assert.AreEqual("uc", result);
         }
     }
 }
--- ServerRealization/ServerCommands.cs
+++ ServerRealization/ServerCommands.cs
@@ -1,5 +1,7 @@
 ﻿using ServerCore;
 using ServerRealization.Database;
+using ServerRealization.Database.Context;
+using System;
 using System.Linq;
 
 namespace ServerRealization
@@ -13,6 +15,7 @@ namespace ServerRealization
                 default: return "message is received";
                 case "cc": return "cs";
                 case "clp": return CheckLoginPassword(args);
+                case "rnu": return RegisterNewUser(args);
             }
         }
 
@@ -26,6 +29,24 @@ namespace ServerRealization
                             .Where((x) => x.Login == args[0] && x.Password == args[1])
                             .Count()
                             == 1).ToString();
+            return "ae";
+        }
+
+        private string RegisterNewUser(string[] args)
+        {
+            if(args != null)
+                if(args.Length > 2)
+                    if(args[0] != "" && args[1] != "" && args[2] != "")
+                    {
+                        if (DBContext.Users
+                            .Where(x => x.Login == args[0])
+                            .Count() >= 1)
+                            return "uc";
+
+                        User user = new User(args[2], args[0], args[1], DateTime.Now);
+                        DBContext.Users.Add(user);
+                        return "True";
+                    }
             return "ae";
         }
     }
