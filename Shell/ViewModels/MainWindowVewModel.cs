a4253aca434452c7d5099085f7b07659d8595e5c
DmitryTahoma <dimon.otrishk@gmail.com>
2020-01-17 12:13:14 +0200

    added transition to the registration page

--- Shell/ViewModels/MainWindowVewModel.cs
+++ Shell/ViewModels/MainWindowVewModel.cs
@@ -1,35 +1,40 @@
 ﻿namespace Shell.ViewModels
 {
+    using Catel.Data;
     using Catel.MVVM;
-    using System.Threading.Tasks;
+    using Shell.Pages;
+    using System.Windows.Controls;
 
     public class MainWindowVewModel : ViewModelBase
     {
         public MainWindowVewModel()
         {
-
+            CurrentPage = new LoginPage();
+            LoginPageContext = ((LoginPage)CurrentPage).DataContext;
+            LoginPageContext.IfNotRegistered += () => 
+            {
+                CurrentPage = new RegistrationPage();
+            };
+            RegistrationPageContext = new RegistrationPageViewModel();
         }
 
         #region Properties
+
+        public LoginPageViewModel LoginPageContext { set; get; }
+
+        public RegistrationPageViewModel RegistrationPageContext { set; get; }
+
+        public Page CurrentPage
+        {
+            get { return GetValue<Page>(CurrentPageProperty); }
+            set { SetValue(CurrentPageProperty, value); }
+        }
+        public static readonly PropertyData CurrentPageProperty = RegisterProperty(nameof(CurrentPage), typeof(Page));
 
         #endregion
 
         #region Commands
 
         #endregion
-
-        protected override async Task InitializeAsync()
-        {
-            await base.InitializeAsync();
-
-            // TODO: subscribe to events here
-        }
-
-        protected override async Task CloseAsync()
-        {
-            // TODO: unsubscribe from events here
-
-            await base.CloseAsync();
-        }
     }
 }
