b5af45b175f97fc33f55ad00b7972b75fb446fa8
DmitryTahoma <dimon.otrishk@gmail.com>
2020-01-18 17:43:13 +0200

    added SignIn in ShellModel

--- ShellModel.Test/DBHelper.Test.cs
+++ ShellModel.Test/DBHelper.Test.cs
@@ -2,6 +2,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ClientCore;
 using ServerRealization;
+using System;
 
 namespace ShellModel.Test
 {
@@ -25,6 +26,42 @@ namespace ShellModel.Test
             string result = client.SendCommand("clp", new string[] { login, password });
 
             Assert.AreEqual("True", result);
+        }
+
+        [DataTestMethod]
+        [DataRow("Alex92", "pass1234", true)]
+        [DataRow("Alex92", "pass12345", false)]
+        [DataRow("Alex926", "pass1234", false)]
+        public void SignInTest(string login, string password, bool expectedResult)
+        {
+            SocketSettings.SocketSettings settings = new SocketSettings.SocketSettings("192.168.0.105", 11221, new int[] { 11222, 11224, 12550 }, 3000);
+            ServerProgram server = new ServerProgram("192.168.0.105", 11221, new int[] { 11222, 11224, 12550 }, 3000);
+            server.Run();
+
+            DBHelper helper = new DBHelper(settings);
+            bool isHappened = helper.SignIn(login, password);
+            Assert.AreEqual(expectedResult, isHappened);
+
+            Client client = new Client(settings);
+            string result = client.SendCommand("clp", new string[] { login, password });
+
+            Assert.AreEqual(result, isHappened.ToString());
+        }
+
+        [TestMethod]
+        public void ActionsIsLockedTest()
+        {
+            SocketSettings.SocketSettings settings = new SocketSettings.SocketSettings("192.168.0.105", 11221, new int[] { 11222, 11224, 12550 }, 3000);
+            ServerProgram server = new ServerProgram("192.168.0.105", 11221, new int[] { 11222, 11224, 12550 }, 3000);
+            server.Run();
+
+            DBHelper helper = new DBHelper(settings);
+            DateTime before = DateTime.Now;
+            helper.Registration("Temp", "TempTemp", "Temp");
+            helper.SignIn("Temp", "Temp");
+            DateTime after = DateTime.Now;
+
+            Assert.IsTrue((after - before).TotalMilliseconds > 200);
         }
     }
 }
--- ShellModel/DBHelper.cs
+++ ShellModel/DBHelper.cs
@@ -6,8 +6,10 @@ namespace Shell.Models
 {
     public class DBHelper
     {
-        Client client;
         object locker = new object();
+        delegate object ProcessAction();
+
+        Client client;
         int delay;
 
         public DBHelper(ISocketSettings settings)
@@ -16,22 +18,21 @@ namespace Shell.Models
             delay = settings.MlsOfDelay;
         }
 
-        public bool Registration(string login, string password, string name)
+        private object DoLockedProcess(ProcessAction action)
         {
-            bool result = false;
-            lock(locker)
+            object result = null;
+            lock (locker)
             {
                 bool isSended = false;
-                Thread clientThread = new Thread(new ThreadStart(() => 
+                Thread clientThread = new Thread(new ThreadStart(() =>
                 {
-                    result = bool.Parse( 
-                        client.SendCommand("rnu", new string[] { login, password, name }));
+                    result = action.Invoke();
                     isSended = true;
                 }));
                 clientThread.Start();
 
                 int counter = 0;
-                while(counter * 100 <= delay)
+                while (counter * 100 <= delay)
                 {
                     Thread.Sleep(100);
                     counter++;
@@ -42,6 +43,32 @@ namespace Shell.Models
                     clientThread.Abort();
             }
             return result;
+        }
+
+        public bool Registration(string login, string password, string name)
+        {
+            object result = DoLockedProcess(() => 
+            {
+                return client.SendCommand("rnu", new string[] { login, password, name });
+            });
+            if (result != null)
+                if (result is string)
+                    if (bool.TryParse(result.ToString(), out bool x))
+                        return x;
+            return false;
+        }
+
+        public bool SignIn(string login, string password)
+        {
+            object result = DoLockedProcess(() =>
+            {
+                return client.SendCommand("clp", new string[] { login, password });
+            });
+            if (result != null)
+                if (result is string)
+                    if (bool.TryParse(result.ToString(), out bool x))
+                        return x;
+            return false;
         }
     }
 }
