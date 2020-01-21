203f15236f358bb711c60daf4a985ac1bf75af57
DmitryTahoma <dimon.otrishk@gmail.com>
2020-01-21 18:36:50 +0200

    added user control for note

---
+++ Shell/Models/Note.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Shell.Models
+{
+    public class Note
+    {
+        public Note(int id, int userId, int stereotypeId, string name, string text, DateTime created, DateTime lastChanged)
+        {
+            Id = id;
+            UserId = userId;
+            StereotypeId = stereotypeId;
+            Name = name;
+            Text = text;
+            Created = created;
+            LastChanged = lastChanged;
+        }
+
+        public int Id { private set; get; }
+        public int UserId { private set; get; }
+        public int StereotypeId { private set; get; }
+        public string Name { set; get; }
+        public string Text { set; get; }
+        public DateTime Created { set; get; }
+        public DateTime LastChanged { set; get; }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Note))
+                return false;
+            Note other = (Note)obj;
+            return this.Id == other.Id
+                && this.Name == other.Name
+                && this.Text == other.Text
+                && this.UserId == other.UserId
+                && this.StereotypeId == other.StereotypeId;
+        }
+    }
+}
---
+++ Shell/ViewModels/SimpleNoteViewModel.cs
@@ -0,0 +1,26 @@
+﻿namespace Shell.ViewModels
+{
+    using Catel.MVVM;
+    using Shell.Models;
+    using System;
+
+    public class SimpleNoteViewModel : ViewModelBase
+    {
+        public SimpleNoteViewModel()
+        {
+            Note = new Note(0, 0, 0, "Note not loaded", "Note not loaded", DateTime.Now, DateTime.Now);
+        }
+
+        #region Properties
+
+        public Note Note { set; get; }
+
+        public string Text { get => Note.Text; }
+
+        public string Name { get => Note.Name; }
+
+        public string LastChanged { get => Note.LastChanged.ToString("dddd, dd MMMM yyyy HH:mm:ss"); }
+
+        #endregion
+    }
+}
