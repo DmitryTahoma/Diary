5da62cbf88580bdd2e2af9d532e01f5cb68014ac
DmitryTahoma <dimon.otrishk@gmail.com>
2020-01-22 16:16:30 +0200

    unnecessary delay in the ServerCore.Test is removed

--- ServerCore.Test/Server.Test.cs
+++ ServerCore.Test/Server.Test.cs
@@ -13,11 +13,11 @@ namespace ServerCore.Test
     public partial class ServerTest
     {
         [DataTestMethod]
-        [DataRow("hello, server", 11222, "192.168.0.106", 11221, "hello, client")]
+        [DataRow("hello, server", 11222, "192.168.0.107", 11221, "hello, client")]
         [DataRow("hi, server", 11322, "127.0.0.1", 11321, "message is received")]
         public void ServerIsRunning(string message, int clientPort, string serverIP, int serverPort, string expectedResponce)
         {
-            Server server = new Server(new TestCommands(), serverIP, serverPort, new int[] { clientPort }, 1000);
+            Server server = new Server(new TestCommands(), serverIP, serverPort, new int[] { clientPort }, 100);
             Thread serverThread = new Thread(new ThreadStart(server.Run));
             serverThread.Start();
 
@@ -38,13 +38,13 @@ namespace ServerCore.Test
         [TestMethod]
         public void ServerIsStops()
         {
-            Server server = new Server(new TestCommands());
+            Server server = new Server(new TestCommands(), new SocketSettings.SocketSettings("127.0.0.1", 11221, new int[] { 11222 }, 300));
             Thread serverThread = new Thread(new ThreadStart(server.Run));
             serverThread.Start();
-            Thread.Sleep(100);//time to start the server
+            Thread.Sleep(100);
             Thread serverThread2 = new Thread(new ThreadStart(server.Stop));
             serverThread2.Start();
-            Thread.Sleep(31000);//time for safe server shutdown
+            Thread.Sleep(400);
             try
             {
                 Assert.AreNotEqual("message is received",
@@ -59,13 +59,13 @@ namespace ServerCore.Test
         [TestMethod]
         public void VoidCommands()
         {
-            Server server = new Server(null);
+            Server server = new Server(null, new SocketSettings.SocketSettings("127.0.0.1", 11221, new int[] { 11222 }, 300));
             Thread serverThread = new Thread(new ThreadStart(server.Run));
             serverThread.Start();
             try
             {
                 Assert.AreEqual("c0", 
-                    Send("what you can?", new TcpClient(getLocalIP(), 11221), getLocalIP()));
+                    Send("what you can?", new TcpClient("127.0.0.1", 11221), "127.0.0.1"));
             }
             finally
             {
@@ -78,8 +78,8 @@ namespace ServerCore.Test
         [DataRow("192.168.0.116", 11223, 11250)]
         public void IsSingleton(string ip, int serverPort, int clientPort)
         {
-            Server server = new Server(null, new SocketSettings.SocketSettings(ip, serverPort, new int[] { clientPort }, 3000));
-            Server server2 = new Server(null, new SocketSettings.SocketSettings(ip, serverPort, new int[] { clientPort }, 3000));
+            Server server = new Server(null, new SocketSettings.SocketSettings(ip, serverPort, new int[] { clientPort }, 300));
+            Server server2 = new Server(null, new SocketSettings.SocketSettings(ip, serverPort, new int[] { clientPort }, 300));
             try
             {
                 Thread serverThread = new Thread(new ThreadStart(() => { server.Run(); }));
@@ -103,7 +103,7 @@ namespace ServerCore.Test
         [TestMethod]
         public void ServerRespondsToAnotherPort()
         {
-            Server server = new Server(null);
+            Server server = new Server(null, new SocketSettings.SocketSettings("127.0.0.1", 11221, new int[] { 11222 }, 300));
             Thread serverThread = new Thread(new ThreadStart(server.Run));
             serverThread.Start();
             Thread.Sleep(100);
@@ -112,21 +112,21 @@ namespace ServerCore.Test
                 //standard port
                 Task client1 = Task.Factory.StartNew(() => { 
                     Assert.AreEqual("c0",
-                        Send("test1", new TcpClient(getLocalIP(), 11221), getLocalIP()));
+                        Send("test1", new TcpClient("127.0.0.1", 11221), "127.0.0.1"));
                 });
                 client1.Wait(2000);
                 Assert.IsTrue(client1.IsCompleted);
                 //another ports
                 Task client2 = Task.Factory.StartNew(() => {
                     Assert.AreEqual("c0",
-                        Send("test1", new TcpClient(getLocalIP(), 11221), getLocalIP(), 12345));
+                        Send("test1", new TcpClient("127.0.0.1", 11221), "127.0.0.1", 12345));
                 });
                 client2.Wait(2000);
                 Assert.IsTrue(client2.IsCompleted);
 
                 Task client3 = Task.Factory.StartNew(() => {
                     Assert.AreEqual("c0",
-                    Send("test1", new TcpClient(getLocalIP(), 11221), getLocalIP(), 18535));
+                    Send("test1", new TcpClient("127.0.0.1", 11221), "127.0.0.1", 18535));
                 });
                 client3.Wait(2000);
                 Assert.IsTrue(client3.IsCompleted);
@@ -141,7 +141,7 @@ namespace ServerCore.Test
         [TestMethod]
         public void ServerIsLogging()
         {
-            Server server = new Server(null);
+            Server server = new Server(null, new SocketSettings.SocketSettings("127.0.0.1", 11221, new int[] { 11222 }, 300));
             Thread serverThread = new Thread(new ThreadStart(server.Run));
             serverThread.Start();
             Thread.Sleep(100);
@@ -155,11 +155,11 @@ namespace ServerCore.Test
                 Assert.AreEqual(EntryLevel.Server, serverLogger.Logs[1].Level);
 
                 Task client = Task.Factory.StartNew(() => {
-                    Send("test", new TcpClient(getLocalIP(), 11221), getLocalIP());
+                    Send("test", new TcpClient("127.0.0.1", 11221), "127.0.0.1");
                 });
                 client.Wait(2000);
 
-                Assert.AreEqual("CLIENT: " + getLocalIP() + ":11222 REQUEST: test RESPONSE: c0", serverLogger.Logs.LastByTime().Text);
+                Assert.AreEqual("CLIENT: " + "127.0.0.1" + ":11222 REQUEST: test RESPONSE: c0", serverLogger.Logs.LastByTime().Text);
                 Assert.AreEqual(EntryLevel.User, serverLogger.Logs.LastByTime().Level);
             }
             finally
@@ -170,10 +170,10 @@ namespace ServerCore.Test
         }
 
         [DataTestMethod]
-        [DataRow("smth", "192.168.0.106", 10000, 10001, 1000)]
-        [DataRow("message", "127.0.0.1", 5067, 5066, 600)]
-        [DataRow("smth", "192.168.0.106", 10000, -1, 1000)]
-        [DataRow("message", "127.0.0.1", 5067, -1, 600)]
+        [DataRow("smth", "192.168.0.107", 10000, 10001, 550)]
+        [DataRow("message", "127.0.0.1", 5067, 5066, 300)]
+        [DataRow("smth", "192.168.0.107", 10000, -1, 100)]
+        [DataRow("message", "127.0.0.1", 5067, -1, 300)]
         public void ClientWaitResponseWithDelayTest(string message, string ip, int port, int clientPort, int mls)
         {
             if (clientPort == -1)
@@ -189,7 +189,7 @@ namespace ServerCore.Test
                 Task client = Task.Factory.StartNew(() => {
                     result = SendAndWaitResponseWithDelay(message, ip, port, clientPort, mls / 2);
                 });
-                client.Wait(mls / 2 + 1000);
+                client.Wait(2000);
 
                 Assert.AreEqual("c0", result);
             }
@@ -201,9 +201,9 @@ namespace ServerCore.Test
         }
 
         [DataTestMethod]
-        [DataRow("smth", "192.168.0.106", 10000, 10001, 1000)]
+        [DataRow("smth", "192.168.0.107", 10000, 10001, 550)]
         [DataRow("message", "127.0.0.1", 5067, 5066, 600)]
-        [DataRow("smth", "192.168.0.106", 10000, -1, 1000)]
+        [DataRow("smth", "192.168.0.107", 10000, -1, 100)]
         [DataRow("message", "127.0.0.1", 5067, -1, 600)]
         public void ClientNotWaitResponseTest(string message, string ip, int port, int clientPort, int mls)
         {
@@ -243,7 +243,7 @@ namespace ServerCore.Test
         [DataRow("127.0.0.1", 11211, 11311, "multiplyAll", new string[] { "5", "5", "5", "3" }, "375")]
         public void ServerIsProcessesParameters(string ip, int port, int clientPort, string command, string[] parameters, string expectedResult)
         {
-            SocketSettings.SocketSettings settings = new SocketSettings.SocketSettings(ip, port, new int[] { clientPort }, 1000);
+            SocketSettings.SocketSettings settings = new SocketSettings.SocketSettings(ip, port, new int[] { clientPort }, 100);
             Server server = new Server(new TestCommands(), settings);
             Thread serverThread = new Thread(server.Run);
             serverThread.Start();
@@ -257,9 +257,10 @@ namespace ServerCore.Test
                         message += "|" + parameters[i];
 
                 string result = "";
-                Thread clientThread = new Thread(() => { result = client.Send(message); });
-                clientThread.Start();
-                Thread.Sleep(500);
+                Task clientTask = Task.Factory.StartNew(() => { 
+                    result = client.Send(message);
+                });
+                clientTask.Wait(2000);
 
                 Assert.AreEqual(expectedResult, result);
             }
