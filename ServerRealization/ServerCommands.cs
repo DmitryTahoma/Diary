b9ba538c80bdcf3f56e145061806a97495528217
DmitryTahoma <dimon.otrishk@gmail.com>
2020-01-22 15:10:46 +0200

    validation of args in ServerCommands made in a separate method

--- ServerRealization/ServerCommands.cs
+++ ServerRealization/ServerCommands.cs
@@ -23,82 +23,84 @@ namespace ServerRealization
 
         private string CheckLoginPassword(string[] args)
         {
-            if(args != null)
-                if(args.Length > 1)
-                    if(args[0] != "" && args[1] != "")
-                        return 
-                            (DBContext.Users
-                            .Where((x) => x.Login == args[0] && x.Password == args[1])
-                            .Count()
-                            == 1).ToString();
+            if(CheckArgs(args, 2))
+                return 
+                    (DBContext.Users
+                    .Where((x) => x.Login == args[0] && x.Password == args[1])
+                    .Count()
+                    == 1).ToString();
             return "ae";
         }
 
         private string RegisterNewUser(string[] args)
         {
-            if(args != null)
-                if(args.Length > 2)
-                    if(args[0] != "" && args[1] != "" && args[2] != "")
-                    {
-                        if (DBContext.Users
-                            .Where(x => x.Login == args[0])
-                            .Count() >= 1)
-                            return "uc";
+            if (!CheckArgs(args, 3))
+                return "ae";
+            
+            if (DBContext.Users
+                .Where(x => x.Login == args[0])
+                .Count() >= 1)
+                return "uc";
 
-                        User user = new User(args[2], args[0], args[1], DateTime.Now);
-                        DBContext.Users.Add(user);
-                        return "True";
-                    }
-            return "ae";
+            User user = new User(args[2], args[0], args[1], DateTime.Now);
+            DBContext.Users.Add(user);
+            return "True";
         }
 
         private string CreateNewNote(string[] args)
         {
-            if(args != null)
-                if(args.Length > 2)
-                    if(args[0] != "" && args[1] != "" && args[2] != "")
-                    {
-                        if (DBContext.Users
-                            .Where(x => x.Login == args[0] && x.Password == args[1])
-                            .Count() != 1)
-                            return "False";
+            if (!CheckArgs(args, 3))
+                return "ae";
+            
+            if (DBContext.Users
+                .Where(x => x.Login == args[0] && x.Password == args[1])
+                .Count() != 1)
+                return "False";
 
-                        string text = "";
-                        if (args.Length >= 4)
-                            text = args[3];
+            string text = "";
+            if (args.Length >= 4)
+                text = args[3];
 
-                        DateTime created = DateTime.Now;
-                        Note note = new Note(DBContext.Users.Where(x => x.Login == args[0]).First(),
-                            DBContext.Collections.Where(x => x.Id == 1).First(),
-                            args[2], text, created, created);
-                        DBContext.Notes.Add(note);
-                        return note.Id.ToString();
-                    }
-            return "ae";
+            DateTime created = DateTime.Now;
+            Note note = new Note(DBContext.Users.Where(x => x.Login == args[0]).First(),
+                DBContext.Collections.Where(x => x.Id == 1).First(),
+                args[2], text, created, created);
+            DBContext.Notes.Add(note);
+            return note.Id.ToString();
         }
 
         private string AddTextToNote(string[] args)
         {
-            if(args != null)
-                if(args.Length > 3)
-                    if(args[0] != "" && args[1] != "" && args[2] != "" && args[3] != "")
-                    {
-                        if (DBContext.Users
-                            .Where(x => x.Login == args[0] && x.Password == args[1])
-                            .Count() != 1)
-                            return "False";
+            if (!CheckArgs(args, 4))
+                return "ae";
+            
+            if (DBContext.Users
+                .Where(x => x.Login == args[0] && x.Password == args[1])
+                .Count() != 1)
+                return "False";
 
-                        if (int.TryParse(args[2], out int id))
-                            if (DBContext.Notes
-                                .Where(x => x.Id == id).Count() == 1)
-                            {
-                                DBContext.Notes
-                                    .Where(x => x.Id == id).First().Text += args[3];
-                                return "True";
-                            }
-                        return "False";
-                    }
-            return "ae";
+            if (int.TryParse(args[2], out int id))
+                if (DBContext.Notes
+                    .Where(x => x.Id == id).Count() == 1)
+                {
+                    DBContext.Notes
+                        .Where(x => x.Id == id).First().Text += args[3];
+                    return "True";
+                }
+            return "False";
+        }
+
+        private bool CheckArgs(string[] args, int expectedCount)
+        {
+            int i = -1;
+            if (args != null)
+                if (args.Length > expectedCount - 1)
+                    for (i = 0; i < expectedCount; ++i)
+                        if (args[i] == "")
+                            return false;
+            if (i == expectedCount)
+                return true;
+            return false;
         }
     }
 }
