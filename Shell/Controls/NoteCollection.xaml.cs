2e6bdfa3d3139c5ca5ab6cc7f8776b3423dc2461
DmitryTahoma <dimon.otrishk@gmail.com>
2020-01-23 20:42:04 +0200

    added NoteCollection

---
+++ Shell/Controls/NoteCollection.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Shell.Controls
+{
+    /// <summary>
+    /// Логика взаимодействия для NoteCollection.xaml
+    /// </summary>
+    public partial class NoteCollection : UserControl
+    {
+        public NoteCollection()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Shell/ViewModels/NoteCollectionViewModel.cs
@@ -0,0 +1,29 @@
+﻿namespace Shell.ViewModels
+{
+    using Catel.MVVM;
+    using Shell.Controls;
+    using System.Windows.Controls;
+
+    public class NoteCollectionViewModel : ViewModelBase
+    {
+        public NoteCollectionViewModel()
+        {
+        AddNote = new Command<StackPanel>(OnAddNoteExecute);
+        }
+
+        #region Properties
+
+        #endregion
+
+        #region Commands
+
+
+        public Command<StackPanel> AddNote { get; private set; }
+        private void OnAddNoteExecute(StackPanel stackPanel)
+        {
+            stackPanel.Children.Add(new SimpleNote());
+        }
+
+        #endregion
+    }
+}
