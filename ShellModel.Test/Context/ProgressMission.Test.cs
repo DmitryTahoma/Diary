85121c8c15907e304ff0e0073718d8a4b155a46e
DmitryTahoma <dimon.otrishk@gmail.com>
2020-01-26 15:44:16 +0200

    added ProgressMession to the ShellModel

---
+++ ShellModel.Test/Context/ProgressMission.Test.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace ShellModel.Context.Test
+{
+    [TestClass]
+    public class ProgressMissionTest
+    {
+        [DataTestMethod]
+        [DataRow(1, 2, 3, 4, 5, "Hello", "World", 1, 2, 3, 4, 5, "Hello", "World", true)]
+        [DataRow(6, 7, 7, 5, 85, "123456", "qwerty", 6, 7, 7, 5, 85, "123456", "qwerty", true)]
+        [DataRow(12, 2, 3, 4, 5, "Hello", "World", 1, 2, 3, 4, 5, "Hello", "World", false)]
+        [DataRow(1, 22, 3, 4, 5, "Hello", "World", 1, 2, 3, 4, 5, "Hello", "World", false)]
+        [DataRow(1, 2, 33, 4, 5, "Hello", "World", 1, 2, 3, 4, 5, "Hello", "World", false)]
+        [DataRow(1, 2, 3, 48, 5, "Hello", "World", 1, 2, 3, 4, 5, "Hello", "World", false)]
+        [DataRow(1, 2, 3, 4, 59, "Hello", "World", 1, 2, 3, 4, 5, "Hello", "World", false)]
+        [DataRow(1, 2, 3, 4, 5, "Heello", "World", 1, 2, 3, 4, 5, "Hello", "World", false)]
+        [DataRow(1, 2, 3, 4, 5, "Hello", "Worldd", 1, 2, 3, 4, 5, "Hello", "World", false)]
+        public void EqualsTest(int id1, int id2, int id3, int id4, int id5, string name, string text, int id1_2, int id2_2, int id3_2, int id4_2, int id5_2, string name_2, string text_2, bool expectedResult)
+        {
+            DateTime t = DateTime.Now;
+            ProgressMission mission1 = new ProgressMission(id1, id2, id3, id4, id5, name, text, t, t, t, t);
+            ProgressMission mission2 = new ProgressMission(id1_2, id2_2, id3_2, id4_2, id5_2, name_2, text_2, t, t, t, t);
+
+            Assert.AreEqual(expectedResult, mission1.Equals(mission2));
+        }
+    }
+}
---
+++ ShellModel/Context/ProgressMission.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ShellModel.Context
+{
+    public class ProgressMission : Mission, IMission
+    {
+        public ProgressMission(int id, int contextId, int actionId, int noteId, int stereotypeId, string name, string text, DateTime created, DateTime lastChanged, DateTime start, DateTime end) 
+            : base(id, MissionType.Progress, contextId, actionId, noteId, stereotypeId, name, text, created, lastChanged, start, end)
+        {
+        }
+
+        public IMissionContext Context { private set; get; }
+        public Progress Progress { get => (Progress)Context; }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is ProgressMission))
+                return false;
+            ProgressMission other = (ProgressMission)obj;
+            return base.Equals(obj)
+                && this.Id == other.Id
+                && this.ContextId == other.ContextId;
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = 1063810044;
+            hashCode = hashCode * -1521134295 + base.GetHashCode();
+            hashCode = hashCode * -1521134295 + EqualityComparer<Progress>.Default.GetHashCode(Progress);
+            return hashCode;
+        }
+    }
+}
