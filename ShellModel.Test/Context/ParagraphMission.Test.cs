5524e714be6b28fea59c1bddc4f7eb211d9b8930
DmitryTahoma <dimon.otrishk@gmail.com>
2020-01-28 13:00:28 +0200

    added ParagraphMission to the ShellModel

---
+++ ShellModel.Test/Context/ParagraphMission.Test.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+
+namespace ShellModel.Context.Test
+{
+    [TestClass]
+    public class ParagraphMissionTest
+    {
+        [DataTestMethod]
+        [DataRow(0, 1, 2, 3, 4, "Name", "Text", new string[] { "Lorem ipsum dolor", "sit amet, consectetur", "adipiscing elit, sed do eiusmod" })]
+        [DataRow(10, 21, 32, 443, 53524, "gdsshdfh", "dgsdhsdfg", new string[] { "tempor incididunt ut labore", "et dolore magna aliqua. Ut enim", "ad minim veniam, quis nostrud exercitation", "ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur." })]
+        public void InitializationTest(int id, int cid, int aid, int nid, int sid, string name, string text, string[] items)
+        {
+            ParagraphMission mission = new ParagraphMission(id, cid, aid, nid, sid, name, text, DateTime.Now, DateTime.Now, DateTime.Now, DateTime.Now);
+            Assert.AreEqual(id, mission.Id);
+            Assert.AreEqual(cid, mission.ContextId);
+            Assert.AreEqual(aid, mission.ActionId);
+            Assert.AreEqual(nid, mission.NoteId);
+            Assert.AreEqual(sid, mission.StereotypeId);
+            Assert.AreEqual(name, mission.Name);
+            Assert.AreEqual(text, mission.Text);
+            Assert.IsTrue(mission is IMission && mission is Mission && mission.Type == MissionType.Paragraph);
+
+            Paragraph paragraph = new Paragraph(cid, items.ToList());
+            ParagraphMission mission2 = new ParagraphMission(id, paragraph, aid, nid, sid, name, text, DateTime.Now, DateTime.Now, DateTime.Now, DateTime.Now);
+            Assert.AreEqual(items.Length, mission2.Context.Count);
+            Assert.AreEqual(paragraph, mission2.Context);
+            Assert.AreEqual(paragraph, mission2.Paragraph);
+        }
+    }
+}
---
+++ ShellModel/Context/ParagraphMission.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ShellModel.Context
+{
+    public class ParagraphMission : Mission, IMission
+    {
+        public ParagraphMission(int id, int contextId, int actionId, int noteId, int stereotypeId, string name, string text, DateTime created, DateTime lastChanged, DateTime start, DateTime end)
+            : base(id, MissionType.Paragraph, contextId, actionId, noteId, stereotypeId, name, text, created, lastChanged, start, end)
+        { }
+
+        public ParagraphMission(int id, Paragraph paragraph, int actionId, int noteId, int stereotypeId, string name,string text, DateTime created, DateTime lastChanged, DateTime start, DateTime end)
+            : base (id, MissionType.Paragraph, paragraph.Id, actionId, noteId, stereotypeId, name, text, created, lastChanged, start, end)
+        {
+            Context = paragraph;
+        }
+
+        public IMissionContext Context { private set; get; }
+        public Paragraph Paragraph { get => (Paragraph)Context; }
+    }
+}
