7fa5a631f03b13469d24554618bcb148bf315ec2
DmitryTahoma <dimon.otrishk@gmail.com>
2020-02-05 11:39:30 +0200

    added CheckTextBox

---
+++ Shell/Controls/CheckTextBox.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Shell.Controls
+{
+    /// <summary>
+    /// Логика взаимодействия для CheckTextBox.xaml
+    /// </summary>
+    public partial class CheckTextBox : UserControl
+    {
+        public CheckTextBox()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Shell/ViewModels/CheckTextBoxViewModel.cs
@@ -0,0 +1,35 @@
+﻿namespace Shell.ViewModels
+{
+    using Catel.Data;
+    using Catel.MVVM;
+    using System.Windows;
+
+    public class CheckTextBoxViewModel : ViewModelBase
+    {
+        public CheckTextBoxViewModel()
+        {            
+        }
+
+        #region Properties
+
+        public bool IsChecked
+        {
+            get { return GetValue<bool>(IsCheckedProperty); }
+            set { SetValue(IsCheckedProperty, value); Decorations = IsChecked ? TextDecorations.Strikethrough : TextDecorations.Baseline; }
+        }
+        public static readonly PropertyData IsCheckedProperty = RegisterProperty(nameof(IsChecked), typeof(bool), false);
+
+        public TextDecorationCollection Decorations
+        {
+            get { return GetValue<TextDecorationCollection>(DecorationProperty); }
+            set { SetValue(DecorationProperty, value); }
+        }
+        public static readonly PropertyData DecorationProperty = RegisterProperty(nameof(Decorations), typeof(TextDecorationCollection), null);
+
+        #endregion
+
+        #region Commands
+
+        #endregion
+    }
+}
