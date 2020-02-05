093bfc698cc364471cc18bf72a7f8c29f0ae0dcd
DmitryTahoma <dimon.otrishk@gmail.com>
2020-02-05 20:07:19 +0200

    added Week

---
+++ Shell/Controls/Week.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Shell.Controls
+{
+    /// <summary>
+    /// Логика взаимодействия для Week.xaml
+    /// </summary>
+    public partial class Week : UserControl
+    {
+        public Week()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Shell/ViewModels/WeekViewModel.cs
@@ -0,0 +1,45 @@
+﻿namespace Shell.ViewModels
+{
+    using Catel.MVVM;
+    using System;
+    using System.Windows.Controls;
+
+    public class WeekViewModel : ViewModelBase
+    {
+        public WeekViewModel()
+        {
+            CreateDays = new Command<WrapPanel>(OnCreateDaysExecute);
+            Start = DateTime.Now;
+            Length = 7;
+        }
+
+        #region Properties
+
+        public DateTime Start { set; get; }
+
+        public int Length { set; get; }
+
+        #endregion
+
+        #region Commands
+
+        public Command<WrapPanel> CreateDays { get; private set; }
+        private void OnCreateDaysExecute(WrapPanel wrapPanel)
+        {
+            DateTime date = Start;
+            for (int i = 0; i < Length; ++i)
+            {
+                Controls.DayOfWeek day = new Controls.DayOfWeek() 
+                {
+                    Margin = new System.Windows.Thickness(10),
+                    MinWidth = 300,
+                    MinHeight = 100 
+                };
+                day.DataContext.Date = date.AddDays(i);
+                wrapPanel.Children.Add(day);
+            }
+        }
+
+        #endregion
+    }
+}
