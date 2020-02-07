a4a40f621ff1361a30107a04fed1167ee1b16dfe
DmitryTahoma <dimon.otrishk@gmail.com>
2020-02-07 15:01:26 +0200

    added Year

---
+++ Shell/Controls/Year.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Shell.Controls
+{
+    /// <summary>
+    /// Логика взаимодействия для Year.xaml
+    /// </summary>
+    public partial class Year : UserControl
+    {
+        public Year()
+        {
+            InitializeComponent();
+        }
+    }
+}
