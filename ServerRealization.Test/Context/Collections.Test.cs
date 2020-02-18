9ecbba82a4509ba5bfb63797b626aabb775a1d2a
DmitryTahoma <dimon.otrishk@gmail.com>
2020-02-18 18:47:03 +0200

    added initialization, tostring tests of Collection on SR

---
+++ ServerRealization.Test/Context/Collections.Test.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ServerRealization.Database;
+using ServerRealization.Database.Context;
+using System;
+using System.Collections.Generic;
+
+namespace ServerRealization.Context.Test
+{
+    [TestClass]
+    public class CollectionTest
+    {
+        [DataTestMethod]
+        [DataRow(0, 10)]
+        [DataRow(-15, 10)]
+        [DataRow(10, -17)]
+        [DataRow(20, 13)]
+        public void InitializationTest(int id, int count)
+        {
+            Collection collection1 = null, collection2 = new Collection(count), collection3 = new Collection();
+
+            try
+            {
+                collection1 = new Collection(id, count);
+                if (id < 1)
+                    Assert.Fail();
+            }
+            catch(ArgumentException) { }
+
+            if(collection1 != null)
+            {
+                Assert.AreEqual(id, collection1.Id);
+                Assert.AreEqual(count < 0 ? 0 : count, collection1.Count);
+            }
+            Assert.AreEqual(count < 0 ? 0 : count, collection2.Count);
+            Assert.AreEqual(0, collection3.Count);
+        }
+
+        [DataTestMethod]
+        [DataRow(new string[] { "", "" }, new bool[] { true, false })]
+        [DataRow(new string[] { "hello", "world" }, new bool[] { false, true })]
+        [DataRow(new string[] { "Lorem ipsum dolor sit amet,", " consectetur adipiscing elit, sed do eiusmod ", "tempor incididunt ut labore et dolore magna aliqua.", " Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.", " Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu", " fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia", " deserunt mollit anim id est laborum." }, new bool[] { true, false, false, true, true, true, true })]
+        public void ToStringTest(string[] names, bool[] isCheckeds)
+        {
+            Collection collection = new Collection(names.Length);
+            List<Point> points = new List<Point>();
+            DBContext.Collections.Add(collection);
+            for (int i = 0; i < names.Length; ++i)
+            {
+                Point p = new Point(collection, names[i], isCheckeds[i]);
+                DBContext.Points.Add(p);
+                points.Add(p);
+            }
+
+            string result = "\b<sc>\b" + names.Length.ToString() + "\b<sc>\b";
+            for(int i = 0; i < names.Length; ++i)
+            {
+                result += points[i].ToString() + "\b<sc>\b";
+            }
+            Assert.AreEqual(result, collection.ToString());
+        }
+    }
+}
--- ServerRealization/Database/Context/Collection.cs
+++ ServerRealization/Database/Context/Collection.cs
@@ -1,16 +1,22 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ServerRealization.Database.Context
 {
     public class Collection : IDBObject
     {
-        public Collection()
-            : this(DBContext.Collections.Max(x => x.Id) + 1, 0) { }
+        public Collection() : this(0) { }            
+
+        public Collection(int count)
+            : this(DBContext.Collections.Count() == 0 ? 1 : DBContext.Collections.Max(x => x.Id) + 1, count) { }
 
         public Collection(int id, int count)
         {
+            if (id < 1)
+                throw new ArgumentException();
             Id = id;
-            Count = count;
+            Count = count < 0? 0 : count;
         }
 
         public int Id { private set; get; }
@@ -23,6 +29,15 @@ namespace ServerRealization.Database.Context
             Collection other = (Collection)obj;
             return this.Id == other.Id
                 && this.Count == other.Count;
+        }
+
+        public override string ToString()
+        {
+            List<Point> points = DBContext.Points.Where(x => x.ParagraphId == Id).ToList();
+            string result = "\b<sc>\b" + Count.ToString() + "\b<sc>\b";
+            foreach (Point point in points)
+                result += point.ToString() + "\b<sc>\b";
+            return result;
         }
     }
 }
