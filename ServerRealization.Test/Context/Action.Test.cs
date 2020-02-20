7882ee8a6bc3fe781c3bd18da539cdf9c8767afc
DmitryTahoma <dimon.otrishk@gmail.com>
2020-02-20 18:47:48 +0200

    added initialization, tostring tests of Action on SR

---
+++ ServerRealization.Test/Context/Action.Test.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ServerRealization.Database;
+using ServerRealization.Database.Context;
+using System;
+
+namespace ServerRealization.Context.Test
+{
+    [TestClass]
+    public class ActionTest
+    {
+        [DataTestMethod]
+        [DataRow(250, 300, "Lorem", "ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.")]
+        [DataRow(567, 8328, "Malorum", "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")]
+        public void InitializationTest(int id, int noteId, string name, string text)
+        {
+            DBContext.Actions.Clear();
+            User user = new User("Name", "Login", "Password", DateTime.Now);
+            DBContext.Users.Add(user);
+
+            Random random = new Random();
+            DateTime created = DateTime.Now.AddDays(random.NextDouble() * -1);
+            DateTime lastChanged = DateTime.Now;
+            Note note = new Note(noteId, user, new Collection(), name, text, created, lastChanged);
+            DBContext.Notes.Add(note);
+
+            DateTime start = new DateTime(random.Next());
+            DateTime end = new DateTime(random.Next());
+            if(start > end)
+            {
+                DateTime t = start;
+                start = end;
+                end = t;
+            }
+
+            Database.Context.Action action1 = new Database.Context.Action(note, start, end);
+            DBContext.Actions.Add(action1);
+            try
+            {
+                Database.Context.Action _ = new Database.Context.Action(note, end, start);
+                Assert.Fail();
+            }
+            catch(ArgumentException) { }
+
+            Database.Context.Action action2 = new Database.Context.Action(id, note, start, end);
+            try
+            {
+                Database.Context.Action _ = new Database.Context.Action(id, note, end, start);
+                Assert.Fail();
+            }
+            catch(ArgumentException) { }
+
+            Assert.AreEqual(id, action2.Id);
+            Assert.AreEqual(start, action1.Start);
+            Assert.AreEqual(start, action2.Start);
+            Assert.AreEqual(end, action1.End);
+            Assert.AreEqual(end, action2.End);
+        }
+
+        [DataTestMethod]
+        [DataRow(250, 300, "Lorem", "ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.")]
+        [DataRow(567, 8328, "Malorum", "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")]
+        public void ToStringTest(int id, int noteId, string name, string text)
+        {
+            User user = new User("Name", "Login", "Password", DateTime.Now);
+            DBContext.Users.Add(user);
+
+            Random random = new Random();
+            DateTime created = DateTime.Now.AddDays(random.NextDouble() * -1);
+            DateTime lastChanged = DateTime.Now;
+            Note note = new Note(noteId, user, new Collection(), name, text, created, lastChanged);
+            DBContext.Notes.Add(note);
+
+            DateTime start = new DateTime(random.Next());
+            DateTime end = new DateTime(random.Next());
+            if (start > end)
+            {
+                DateTime t = start;
+                start = end;
+                end = t;
+            }
+            Database.Context.Action action = new Database.Context.Action(id, note, start, end);
+
+            Assert.AreEqual("\b<sa>\b" + id.ToString() + "\b<sa>\b" + note.ToString() + "\b<sa>\b" + (start - DateTime.MinValue).TotalDays.ToString() + "\b<sa>\b" + (end - DateTime.MinValue).TotalDays.ToString() + "\b<sa>\b", action.ToString());
+        }
+    }
+}
--- ServerRealization/Database/Context/Action.cs
+++ ServerRealization/Database/Context/Action.cs
@@ -9,13 +9,15 @@ namespace ServerRealization.Database.Context
             : this(DBContext.Notes.Where(x => x.Id == noteId).First(), start, end) { }
 
         public Action(Note note, DateTime start, DateTime end)
-            : this(DBContext.Actions.Max(x => x.Id) + 1, note, start, end) { }
+            : this(DBContext.Actions.Count == 0? 1 : DBContext.Actions.Max(x => x.Id) + 1, note, start, end) { }
 
         public Action(int id, int noteId, DateTime start, DateTime end)
             : this(id, DBContext.Notes.Where(x => x.Id == noteId).First(), start, end) { }
 
         public Action(int id, Note note, DateTime start, DateTime end)
         {
+            if (start > end)
+                throw new ArgumentException();
             Id = id;
             Note = note;
             Start = start;
@@ -38,6 +40,11 @@ namespace ServerRealization.Database.Context
                 && this.Start.Equals(other.Start)
                 && this.End.Equals(other.End)
                 && this.Note.Equals(other.Note);
+        }
+
+        public override string ToString()
+        {
+            return "\b<sa>\b" + Id.ToString() + "\b<sa>\b" + Note.ToString() + "\b<sa>\b" + (Start - DateTime.MinValue).TotalDays.ToString() + "\b<sa>\b" + (End - DateTime.MinValue).TotalDays.ToString() + "\b<sa>\b";
         }
     }
 }
