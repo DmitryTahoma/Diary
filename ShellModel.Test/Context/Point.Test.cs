fba5c6d54f0f1f9c50f591998b8a142365371d75
DmitryTahoma <dimon.otrishk@gmail.com>
2020-02-22 19:07:14 +0200

    added Point to SM

---
+++ ShellModel.Test/Context/Point.Test.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ShellModel.Context.Test
+{
+    [TestClass]
+    public class PointTest
+    {
+        [DataTestMethod]
+        [DataRow(102057, "Lorem ipsum", false)]
+        [DataRow(1103245, "Sed ut perspiciatis", true)]
+        [DataRow(333070, "Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.", false)]
+        [DataRow(2117660545, "Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?", true)]
+        public void InitializationTest(int id, string text, bool isChecked)
+        {
+            Point point1 = new Point(id, text, isChecked);
+            ServerRealization.Database.Context.Point dbPoint = new ServerRealization.Database.Context.Point(id, new ServerRealization.Database.Context.Collection(), text, isChecked);
+            Point point2 = new Point(dbPoint.ToString());
+
+            Assert.AreEqual(id, point1.Id);
+            Assert.AreEqual(id, point2.Id);
+            Assert.AreEqual(text, point1.Text);
+            Assert.AreEqual(text, point2.Text);
+            Assert.AreEqual(isChecked, point1.IsChecked);
+            Assert.AreEqual(isChecked, point2.IsChecked);
+        }
+    }
+}
---
+++ ShellModel/Context/Point.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace ShellModel.Context
+{
+    public class Point
+    {
+        public Point(int id, string text, bool isChecked)
+        {
+            Id = id;
+            Text = text;
+            IsChecked = isChecked;
+        }
+
+        public Point(string dbStr)
+        {
+            string splitter = "\b<sp>\b";
+            Regex regex1 = new Regex("^" + splitter + @"\d+" + splitter + @"[\s\S]*" + splitter + "1" + splitter);
+            Regex regex2 = new Regex("^" + splitter + @"\d+" + splitter + @"[\s\S]*" + splitter);
+            if (regex2.IsMatch(dbStr))
+            {
+                string[] values = new string[2];
+                for(int i = 0, s = 0, index = -1, start = 0; i < dbStr.Length; ++i)
+                {
+                    if (i == dbStr.Length - 1)
+                        values[index] = dbStr.Substring(start, i - start - splitter.Length + 1);
+                    if(s == splitter.Length)
+                    {
+                        if(index != -1)
+                            values[index] = dbStr.Substring(start, i - start - splitter.Length);
+                        index++;
+                        s = 0;
+                        start = i;
+                        if (index == 2)
+                            break;
+                    }
+                    if (splitter[s] == dbStr[i])
+                        s++;
+                    else if (s != 0)
+                        s = 0;
+                }
+                Id = int.Parse(values[0]);
+                Text = values[1];
+                if (regex1.IsMatch(dbStr))
+                    IsChecked = true;
+            }
+            else
+                throw new ArgumentException();
+        }
+
+        public int Id { private set; get; }
+        public string Text { set; get; }
+        public bool IsChecked { set; get; }
+    }
+}
