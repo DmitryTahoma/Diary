c2b543d78d8ba1c90815b26f5f70850561a36e7a
DmitryTahoma <dimon.otrishk@gmail.com>
2020-03-05 17:30:42 +0200

    UserContent moved to the folder of pages

---
+++ Shell/Controls/Pages/UserContent.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Shell.Controls.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для UserContent.xaml
+    /// </summary>
+    public partial class UserContent : UserControl
+    {
+        public UserContent()
+        {
+            InitializeComponent();
+        }
+    }
+}
