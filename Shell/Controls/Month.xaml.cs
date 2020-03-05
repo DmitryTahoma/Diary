11749ee3580cdb5a538fe25f682578a6fd2012ec
DmitryTahoma <dimon.otrishk@gmail.com>
2020-03-05 17:15:10 +0200

    added Month control

---
+++ Shell/Controls/Month.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Shell.Controls
+{
+    /// <summary>
+    /// Логика взаимодействия для Month.xaml
+    /// </summary>
+    public partial class Month : UserControl
+    {
+        public Month()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Shell/ViewModels/MonthViewModel.cs
@@ -0,0 +1,42 @@
+﻿namespace Shell.ViewModels
+{
+    using Catel.MVVM;
+    using Shell.Controls;
+    using System;
+    using System.Windows.Controls;
+
+    public class MonthViewModel : ViewModelBase
+    {
+        public MonthViewModel()
+        {
+            BindStackPanel = new Command<StackPanel>(OnBindStackPanelExecute);            
+        }
+
+        #region Properties
+
+        public int Month { set; get; }
+        
+        public int Year { set; get; }
+
+        #endregion
+
+        #region Commands
+
+        public Command<StackPanel> BindStackPanel { get; private set; }
+        private void OnBindStackPanelExecute(StackPanel stackPanel)
+        {
+            DateTime start = new DateTime(Year, Month, 1);
+            while (start.DayOfWeek != System.DayOfWeek.Monday)
+                start = start.AddDays(-1);
+
+            for(int i = 0; start.AddDays(7 * i).Month != (Month + 1 == 13? 1 : Month + 1); ++i)
+            {
+                Week w = new Week();
+                w.DataContext.Start = start.AddDays(7 * i);
+                stackPanel.Children.Add(w);
+            }
+        }
+
+        #endregion
+    }
+}
