7136978d26b3748967f3a3ac682acbdacbc78e3d
DmitryTahoma <dimon.otrishk@gmail.com>
2020-04-10 23:10:29 +0300

    restyling SimpleNote

--- Shell/ViewModels/SimpleNoteViewModel.cs
+++ Shell/ViewModels/SimpleNoteViewModel.cs
@@ -4,6 +4,7 @@
     using Catel.MVVM;
     using ShellModel.Context;
     using System;
+    using System.Windows;
 
     public class SimpleNoteViewModel : ViewModelBase
     {
@@ -12,12 +13,17 @@
 
         public SimpleNoteViewModel()
         {
-            Note = new Note(-1, "", "", DateTime.MinValue, DateTime.MinValue);
             Delete = new Command(OnDeleteExecute);
+            UpdateNamePH = new Command(OnUpdateNamePHExecute);
+            UpdateTextPH = new Command(OnUpdateTextPHExecute);
+            Note = new Note(-1, "", "", DateTime.MinValue, DateTime.MinValue);
         }
 
         public SimpleNoteViewModel(Note note)
         {
+            Delete = new Command(OnDeleteExecute);
+            UpdateNamePH = new Command(OnUpdateNamePHExecute);
+            UpdateTextPH = new Command(OnUpdateTextPHExecute);
             Note = note;
         }
 
@@ -44,6 +50,20 @@
         }
         public static readonly PropertyData StringLastChangedProperty = RegisterProperty(nameof(StringLastChanged), typeof(string), null);
 
+        public Visibility VisibilityNamePH
+        {
+            get { return GetValue<Visibility>(VisibilityNamePHProperty); }
+            set { SetValue(VisibilityNamePHProperty, value); }
+        }
+        public static readonly PropertyData VisibilityNamePHProperty = RegisterProperty(nameof(VisibilityNamePH), typeof(Visibility), null);
+
+        public Visibility VisibilityTextPH
+        {
+            get { return GetValue<Visibility>(VisibilityTextPHProperty); }
+            set { SetValue(VisibilityTextPHProperty, value); }
+        }
+        public static readonly PropertyData VisibilityTextPHProperty = RegisterProperty(nameof(VisibilityTextPH), typeof(Visibility), null);
+
         #endregion
 
         #region Commands
@@ -52,6 +72,18 @@
         private void OnDeleteExecute()
         {
             Deleting?.Invoke();
+        }
+
+        public Command UpdateNamePH { get; private set; }
+        private void OnUpdateNamePHExecute()
+        {
+            VisibilityNamePH = Note.Name == "" ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public Command UpdateTextPH { get; private set; }
+        private void OnUpdateTextPHExecute()
+        {
+            VisibilityTextPH = Note.Text == "" ? Visibility.Visible : Visibility.Collapsed;
         }
 
         #endregion
