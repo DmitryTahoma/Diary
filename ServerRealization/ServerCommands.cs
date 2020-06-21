df1485419d3e9490da8c8c1c68df1d44e7cf014c
DmitryTahoma <dimon.otrishk@gmail.com>
2020-06-21 11:03:02 +0300

    removed development mode from SR

--- ServerRealization/ServerCommands.cs
+++ ServerRealization/ServerCommands.cs
@@ -31,7 +31,6 @@ namespace ServerRealization
                 case "rp": return RemovePoint(args);
                 case "dn": return DuplicateNote(args);
                 case "dpm": return DuplicateParagraphMission(args);
-                case "generate1000notes": return Generate1000Notes();
             }
         }
 
@@ -313,7 +312,7 @@ namespace ServerRealization
             return result;
         }
 
-        public string RemoveNoteCascade(string[] args)
+        private string RemoveNoteCascade(string[] args)
         {
             if (!ArgsHelper.CheckArgs(args, 3, 2))
                 return "ae";
@@ -345,7 +344,7 @@ namespace ServerRealization
             return "True";
         }
 
-        public string RemovePoint(string[] args)
+        private string RemovePoint(string[] args)
         {
             if (!ArgsHelper.CheckArgs(args, 3, 2))
                 return "ae";
@@ -371,7 +370,7 @@ namespace ServerRealization
             return "True";
         }
 
-        public string DuplicateNote(string[] args)
+        private string DuplicateNote(string[] args)
         {
             if (!ArgsHelper.CheckArgs(args, 6, 2, 3, 4, 5))
                 return "ae";
@@ -439,57 +438,6 @@ namespace ServerRealization
                 result += "|" + dPoint.Id.ToString();
             }
             return result;
-        }
-
-        public string Generate1000Notes()
-        {
-            string login = "Alex92", password = "pass1234";
-            int dispDays = 1, count = 1000;
-            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-            Random random = new Random();
-            int userId = -1;
-            try
-            {
-                userId = DBContext.Users.Where(x => x.Login == login && x.Password == password).First().Id;
-            }
-            catch { return ""; }
-
-            for (int i = 0; i < count; ++i)
-            {
-                DateTime created = DateTime.Now.AddDays(dispDays * (random.Next() % 2 == 0 ? 1 : -1) + random.NextDouble()),
-                         lastChanged = DateTime.Now.AddDays(dispDays * (random.Next() % 2 == 0 ? 1 : -1) + random.NextDouble());
-                if (created > lastChanged)
-                {
-                    DateTime t = created;
-                    created = lastChanged;
-                    lastChanged = t;
-                }
-                Note note = new Note(userId, 1, new string(Enumerable.Repeat(chars, random.Next(10, 40)).Select(s => s[random.Next(s.Length)]).ToArray()), new string(Enumerable.Repeat(chars, random.Next(40, 120)).Select(s2 => s2[random.Next(s2.Length)]).ToArray()), created, lastChanged);
-                DBContext.Notes.Add(note);
-                if (random.Next() % 2 == 0)
-                {
-                    DateTime start = DateTime.Now.AddDays(dispDays * (random.Next() % 2 == 0 ? 1 : -1) + random.NextDouble()),
-                             end = DateTime.Now.AddDays(dispDays * (random.Next() % 2 == 0 ? 1 : -1) + random.NextDouble());
-                    if (start > end)
-                    {
-                        DateTime t = start;
-                        start = end;
-                        end = t;
-                    }
-                    Database.Context.Action action = new Database.Context.Action(note, start, end);
-                    DBContext.Actions.Add(action);
-                    Collection collection = new Collection(random.Next(0, 10));
-                    DBContext.Collections.Add(collection);
-                    for (int j = 0; j < collection.Count; ++j)
-                    {
-                        Point point = new Point(collection, new string(Enumerable.Repeat(chars, random.Next(10, 70)).Select(s => s[random.Next(s.Length)]).ToArray()), random.Next() % 2 == 0);
-                        DBContext.Points.Add(point);
-                    }
-                    Mission mission = new Mission(action, false, collection);
-                    DBContext.Missions.Add(mission);
-                }
-            }
-            return "";
         }
     }
 }
